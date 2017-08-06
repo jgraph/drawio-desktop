1d8b435b14ad928232539d8a8a2dbc4c2e4c96d9
mediaslav <github.mediaslav.com>
2017-08-07 00:03:23 +0300

    build script cleanup

--- sync.js
+++ sync.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-const electronAppDir = path.resolve(__dirname, 'draw.io', 'war')
-const appjsonpath = path.resolve(__dirname, 'draw.io', 'war', 'package.json')
+const electronAppDir = path.join(__dirname, 'draw.io', 'war')
+const appjsonpath = path.join(__dirname, 'draw.io', 'war', 'package.json')
 
-// let ver = fs.readFileSync(path.resolve(__dirname, 'draw.io', 'VERSION'), 'utf8')
-let ver = '6.4.3' // just to test autoupdate
+let ver = fs.readFileSync(path.join(__dirname, 'draw.io', 'VERSION'), 'utf8')
+// let ver = '6.4.3' // just to test autoupdate
 
 let pj = require(appjsonpath)
 
