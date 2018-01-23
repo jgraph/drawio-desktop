d4edffe2ae585be8564050bbd5170b8256f61c25
David Benson <david@jgraph.com>
2018-01-23 20:12:11 +0000

    Moare paths

--- sync.js
+++ sync.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-const electronAppDir = path.join(__dirname, 'draw.io', 'src/main/webapp')
-const appjsonpath = path.join(__dirname, 'draw.io', 'src/main/webapp', 'package.json')
+const electronAppDir = path.join(__dirname, 'drawio', 'src/main/webapp')
+const appjsonpath = path.join(__dirname, 'drawio', 'src/main/webapp', 'package.json')
 
-let ver = fs.readFileSync(path.join(__dirname, 'draw.io', 'VERSION'), 'utf8')
+let ver = fs.readFileSync(path.join(__dirname, 'drawio', 'VERSION'), 'utf8')
 //let ver = '7.0.5' // just to test autoupdate
 
 let pj = require(appjsonpath)
