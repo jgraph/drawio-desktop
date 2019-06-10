3fd31a36b4d4656b76fb2093db4a9d6f6d2e3856
Ashraf Teleb <ashraf.teleb85@gmail.com>
2019-06-10 19:00:50 +0200

    #93 Mac Notarization, #107 wrong exec name in Linux, #105 vsdx file association, #96 svg icon

---
+++ build/notarize.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  const appName = context.packager.appInfo.productFilename;
+
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  return await notarize({
+    appBundleId: 'com.jgraph.drawio.desktop',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
