a23e3332f4bcb5072e08c1c98673acee4b093f03
Ashraf Teleb <ashraf.teleb85@gmail.com>
2019-08-21 11:19:53 -0400

    Added disableUpdate parameter to sync

--- sync.js
+++ sync.js
@@ -4,6 +4,7 @@ const child_process = require('child_process')
 
 const electronAppDir = path.join(__dirname, 'drawio', 'src/main/webapp')
 const appjsonpath = path.join(__dirname, 'drawio', 'src/main/webapp', 'package.json')
+const disableUpdatePath = path.join(__dirname, 'drawio', 'src/main/webapp', 'disableUpdate.js')
 
 let ver = fs.readFileSync(path.join(__dirname, 'drawio', 'VERSION'), 'utf8')
 //let ver = '7.0.5' // just to test autoupdate
@@ -13,5 +14,10 @@ let pj = require(appjsonpath)
 pj.version = ver
 
 fs.writeFileSync(appjsonpath, JSON.stringify(pj, null, 2), 'utf8')
+
+if (process.argv[2] == 'disableUpdate')
+{
+	fs.writeFileSync(disableUpdatePath, 'module.exports = { disableUpdate: function() { return true;}}', 'utf8');
+}
 
 child_process.spawnSync('yarn', ['install', '--production'], {cwd: electronAppDir})
